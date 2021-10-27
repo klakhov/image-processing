f6b257bbb1b4341dc77a8649c61ba910c7e1c77f
Kirill Lakhov <kirill.lakhov@intel.com>
2021-10-27 12:08:29 +0300

    shadows improvements

--- leaves/main.py
+++ leaves/main.py
@@ -5,36 +5,41 @@ import matplotlib.pyplot as plt
 def find_ill_parts(img):
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
     image_erode = cv.erode(img, kernel)
+
     hsv_img = cv.cvtColor(image_erode, cv.COLOR_BGR2HSV)
+    healthy_part = cv.inRange(hsv_img, (36,25,35), (86, 255, 255))
+
     markers = np.zeros((img.shape[0], img.shape[1]), dtype="int32")
-    markers[90:140, 90:140] = 255
+    markers[healthy_part>1] = 255
     markers[236:255, 0:20] = 1
     markers[0:20, 0:20] = 1
     markers[0:20, 236:255] = 1
     markers[236:255, 236:255] = 1
     leafs_area_BGR = cv.watershed(image_erode, markers)
-    healthy_part = cv.inRange(hsv_img, (36,25,25), (86, 255, 255))
     ill_part = leafs_area_BGR - healthy_part
     mask = np.zeros_like(img, np.uint8)
     mask [leafs_area_BGR > 1] = (255 , 0, 255)
     mask[ill_part > 1] = (0, 0, 255)
     return mask
 
-def change_shadows(img):
-    black_pixels = np.where(
-        (img[:, :, 0] < 40) & 
-        (img[:, :, 1] < 40) & 
-        (img[:, :, 2] < 40)
-    )
-    img[black_pixels] = [170, 150, 160]
-    return img
 
+def change_shadows(img):
+    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    b, g, r = cv.split(img[0:20, 235:255])
+    b2, g2, r2 = cv.split(img[235:255, 0:20])
+    b = np.append(b, b2, 1)
+    g = np.append(g, g2, 1)
+    r = np.append(r, r2, 1)
+    black_pixels = np.where(
+        (hsv_img[:, :, 2] < 50)
+    )
+    img[black_pixels] = [np.median(b), np.median(g), np.median(r)]
+    return img
 
 columns = 3
 rows = 1
 fig =  plt.figure(figsize=(10, 7))
-img = cv.imread("1.jpg")
-
+img = cv.imread("2.jpg", cv.IMREAD_COLOR)
 wihout_shadows = change_shadows(img)
 
 gauss = cv.GaussianBlur(wihout_shadows, (7,7), cv.BORDER_DEFAULT)
